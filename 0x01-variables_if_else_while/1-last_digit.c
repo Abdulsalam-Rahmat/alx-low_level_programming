7972fbfe9674a499550ded6226e19780f8c42d8d
root <rahmahpriceless@gmail.com>
2022-03-11 13:53:31 -0800

    fixed betty for 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,8 +20,7 @@ int main(void)
 		       n, m);
 	else if (m == 0)
 		printf("Last digit of %i is %i and is 0\n", n, m);
-	ese printf("Last digit of %i is %i and id less than 6 and not 0\n",
-		   n, m);
-
+	else
+		printf("Last digit of %i is %i and id less than 6 and not 0\n", n, m);
 	return (0);
 }

ae49c8d7255bfbc69169cd6270194a5b9ccfd706
root <rahmahpriceless@gmail.com>
2022-03-17 06:06:24 -0700

    corrected 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -28,7 +28,7 @@ void times_table(void)
 			else
 			{
 				_putchar((n / 10) + '0');
-				_putchar((n %n10) + '0');
+				_putchar((n % 10) + '0');
 				if (j == 9)
 					continue;
 				_putchar(',');

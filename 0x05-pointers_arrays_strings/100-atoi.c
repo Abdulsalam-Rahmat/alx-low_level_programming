bcc0dfd0c61df60a1185232886d8d4673f87a51f
root <rahmahpriceless@gmail.com>
2022-03-21 15:22:51 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * _atoi - int
- *s: pointer
- * Return: int.
+ * _atoi - converts a string to an integer.
+ *@s: input string
+ * Return: integer.
  */
 int _atoi(char *s)
 {
@@ -25,7 +25,7 @@ int _atoi(char *s)
 		}
 		count++;
 	}
-	for (i = count - size; i < count; i++))
+	for (i = count - size; i < count; i++)
 	{
 		oi = oi + ((*(s + i) - 48) * m);
 		m /= 10;
